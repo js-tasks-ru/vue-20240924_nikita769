7be467023c232de03567be486386146ed7f572a4
nepomamac2 <apin2018@mail.ru>
2024-10-22 00:02:58 +0700

    fix: caclulator, selected meetup

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -3,7 +3,6 @@ import { computed, defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'CalculatorApp',
 
-<<<<<<< HEAD
   setup () {
     const a = ref(0)
     const b = ref(0)
@@ -24,85 +23,31 @@ export default defineComponent({
       }
       return undefined
     })
-=======
-  setup() {
-    const a = ref(0)
-    const b = ref(0)
-    const operator = ref('sum')
-    const operations = [
-      {
-        operator: 'sum',
-        label: '➕',
-      },
-      {
-        operator: 'subtract',
-        label: '➖',
-      },
-      {
-        operator: 'multiply',
-        label: '✖',
-      },
-      {
-        operator: 'divide',
-        label: '➗',
-      },
-    ]
-
-    const calculations = {
-      sum: (a, b) => a + b,
-      subtract: (a, b) => a - b,
-      multiply: (a, b) => a * b,
-      divide: (a, b) => a / b,
-    }
-
-    const result = computed(() => calculations[operator.value]?.(a.value, b.value))
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
 
     return {
       a,
       b,
       operator,
       result,
-<<<<<<< HEAD
-=======
-      operations,
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
     }
   },
 
   template: `
     <div class="calculator">
-<<<<<<< HEAD
-    <input type="number" v-model="a" aria-label="First operand" />
-
-    <div class="calculator__operators">
-      <label><input v-model="operator" type="radio" name="operator" value="sum"/>➕</label>
-      <label><input v-model="operator" type="radio" name="operator" value="subtract"/>➖</label>
-      <label><input v-model="operator" type="radio" name="operator" value="multiply"/>✖</label>
-      <label><input v-model="operator" type="radio" name="operator" value="divide"/>➗</label>
-    </div>
-
-    <input type="number" v-model="b" aria-label="Second operand" />
-=======
       <input type="number" v-model="a" aria-label="First operand" />
 
       <div class="calculator__operators">
-        <label v-for="operation in operations">
-          <input v-model="operator" type="radio" name="operator" :value="operation.operator"/>
-          {{ operation.label }}
-        </label>
+        <label><input v-model="operator" type="radio" name="operator" value="sum"/>➕</label>
+        <label><input v-model="operator" type="radio" name="operator" value="subtract"/>➖</label>
+        <label><input v-model="operator" type="radio" name="operator" value="multiply"/>✖</label>
+        <label><input v-model="operator" type="radio" name="operator" value="divide"/>➗</label>
       </div>
 
       <input type="number" v-model="b" aria-label="Second operand" />
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
 
-    <div>=</div>
+      <div>=</div>
 
-<<<<<<< HEAD
-    <output>{{ result }}</output>
-=======
       <output>{{ result }}</output>
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
     </div>
   `,
 })
--- 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
+++ 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
@@ -5,12 +5,6 @@ export default defineComponent({
   name: 'SelectedMeetupApp',
 
   setup() {
-<<<<<<< HEAD
-=======
-    const MIN_ID = 1
-    const MAX_ID = 5
-    const ids = Array.from({ length: MAX_ID }, (_, i) => i + MIN_ID)
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
     const meetupId = ref(1)
     const meetup = ref(null)
 
@@ -23,12 +17,6 @@ export default defineComponent({
     )
 
     return {
-<<<<<<< HEAD
-=======
-      ids,
-      MIN_ID,
-      MAX_ID,
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
       meetupId,
       meetup,
     }
@@ -36,7 +24,6 @@ export default defineComponent({
 
   template: `
     <div class="meetup-selector">
-<<<<<<< HEAD
     <div class="meetup-selector__control">
       <button class="button button--secondary" type="button" :disabled="meetupId <= 1" @click="meetupId -= 1">Предыдущий</button>
 
@@ -62,34 +49,6 @@ export default defineComponent({
     <div v-if="meetup" class="meetup-selector__cover">
       <div class="meetup-cover">
         <h1 class="meetup-cover__title">{{ meetup.title }}</h1>
-=======
-      <div class="meetup-selector__control">
-        <button class="button button--secondary" type="button" :disabled="meetupId <= MIN_ID" @click="meetupId -= 1">Предыдущий</button>
-
-        <div class="radio-group" role="radiogroup">
-          <div v-for="i in ids" class="radio-group__button">
-            <input
-              :id="\`meetup-id-\${i}\`"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              :value="i"
-              v-model="meetupId"
-            />
-            <label :for="\`meetup-id-\${i}\`" class="radio-group__label">
-              {{ i }}
-            </label>
-          </div>
-        </div>
-
-        <button class="button button--secondary" type="button" :disabled="meetupId >= MAX_ID" @click="meetupId += 1">Следующий</button>
-      </div>
-
-      <div class="meetup-selector__cover">
-        <div v-if="meetup" class="meetup-cover">
-          <h1 class="meetup-cover__title">{{ meetup.title }}</h1>
-        </div>
->>>>>>> 30ff7da4a1f7717c7600c562525272d98f219edf
       </div>
     </div>
 
